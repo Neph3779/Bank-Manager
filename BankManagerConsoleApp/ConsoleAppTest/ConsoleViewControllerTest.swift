dc92e41e89ba2e7fb7f6fa6d2502960411a84d3d
Neph <tony3322@naver.com>
2022-10-01 09:32:49 +0900

    [Test] ConsoleViewController 테스트 코드 수정

--- BankManagerConsoleApp/ConsoleAppTest/ConsoleViewControllerTest.swift
+++ BankManagerConsoleApp/ConsoleAppTest/ConsoleViewControllerTest.swift
@@ -8,36 +8,40 @@
 import XCTest
 
 class ConsoleViewControllerTest: XCTestCase {
-    var consoleViewControllerStubUserInput1: ConsoleViewController!
-    var consoleViewControllerStubUserInput2: ConsoleViewController!
-    var consoleViewControllerStubUserInputNot1Or2: ConsoleViewController!
+    var sut: ConsoleViewController!
     
     override func setUpWithError() throws {
-        consoleViewControllerStubUserInput1 = ConsoleViewController(userInput: "1")
-        
-        consoleViewControllerStubUserInput2 = ConsoleViewController(userInput: "2")
-        
-        consoleViewControllerStubUserInputNot1Or2 = ConsoleViewController(userInput: "a")
+        sut = ConsoleViewController()
     }
 
     override func tearDownWithError() throws {
-        consoleViewControllerStubUserInput1 = nil
-        consoleViewControllerStubUserInput2 = nil
-        consoleViewControllerStubUserInputNot1Or2 = nil
+        sut = nil
     }
     
     func test_userInput이_1일때_shouldContinue의_반환값이_success_true인지() {
-        let result = consoleViewControllerStubUserInput1.shouldContinue()
+        // given
+        let userInput = "1"
+        // when
+        let result = sut.shouldContinue(input: userInput)
+        // then
         XCTAssertEqual(result, .success(true))
     }
     
     func test_userInput이_2일때_shouldContinue의_반환값이_success_false인지() {
-        let result = consoleViewControllerStubUserInput2.shouldContinue()
+        // given
+        let userInput = "2"
+        // when
+        let result = sut.shouldContinue(input: userInput)
+        // then
         XCTAssertEqual(result, .success(false))
     }
     
     func test_userInput이_1과_2가_아닐때_shouldContinue의_반환값이_failure이고_invalidUserInput_error을_반환하는지() {
-        let result = consoleViewControllerStubUserInputNot1Or2.shouldContinue()
+        // given
+        let userInput = ""
+        // when
+        let result = sut.shouldContinue(input: userInput)
+        // then
         XCTAssertEqual(result, .failure(.invalidUserInput))
     }
 }
