7bd1bbfc44445ac1e83049fd09405302713884c7
Neph <tony3322@naver.com>
2022-10-19 15:37:05 +0900

    [Fix] 은행원 업무 속도 조절

--- BankManagerUIApp/BankManagerUIApp/Model/Bank.swift
+++ BankManagerUIApp/BankManagerUIApp/Model/Bank.swift
@@ -8,39 +8,6 @@
 import Foundation
 
 struct Bank {
-    private(set) var numberOfBankTeller: Int
-    private(set) var openTime: CFAbsoluteTime?
-    private(set) var closeTime: CFAbsoluteTime?
-
-    init(numberOfBankTeller: Int) {
-        self.numberOfBankTeller = numberOfBankTeller
-    }
-
-    mutating func openBank() {
-        openTime = CFAbsoluteTimeGetCurrent()
-    }
-
-    mutating func closeBank(totalCustomerNumber: Int) throws {
-        closeTime = CFAbsoluteTimeGetCurrent()
-        let spentTime = try getSpentTime()
-
-        print("업무가 마감되었습니다. 오늘 업무를 처리한 고객은 총 \(totalCustomerNumber)명이며, 총 업무시간은 \(spentTime)초입니다.")
-    }
-
-    private func getSpentTime() throws -> String {
-        guard let open = openTime,
-              let close = closeTime else {
-            throw BankManagerError.failToCaclulateSpentTime
-        }
-
-        let spentTime = formatTimeDuration(time: close - open)
-        return spentTime
-    }
-
-    private func formatTimeDuration(time: CFAbsoluteTime) -> String {
-        let timeDuration = time
-        return String(format: "%.2f", timeDuration)
-    }
 }
 
 extension Bank {
@@ -53,11 +20,11 @@ extension Bank {
         var time: useconds_t {
             switch self {
             case .deposit:
-                return 1_100_000
+                return 3_100_000
             case .examineLoanDocument, .loan:
-                return 300_000
+                return 900_000
             case .judgementLoan:
-                return 500_000
+                return 1500_000
             }
         }
     }
