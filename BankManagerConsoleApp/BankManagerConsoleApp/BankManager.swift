7992b202ef886c1dc27aa8ceecf6001b806ee9d3
Neph <tony3322@naver.com>
2022-10-19 16:02:55 +0900

    [Chore] 엉킨 파일 분리, 재생성

--- BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/BankManager.swift
@@ -1,8 +1,73 @@
 //
 //  BankManager.swift
-//  BankManagerConsoleApp
-//
-//  Created by 천수현 on 2022/10/19.
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
 //
 
 import Foundation
+
+struct BankManager {
+    private var bank: Bank
+    private var consoleViewController: ConsoleViewController
+    private var randomGenerator: RandomGenerator
+    private let bankOperationQueue = OperationQueue()
+    private var randomCustomers: [Customer]?
+    private var totalCustomerNumber: Int = 0
+
+    init(bank: Bank, consoleViewer: ConsoleViewController, randomGenerator: RandomGenerator) {
+        self.bank = bank
+        self.consoleViewController = consoleViewer
+        self.randomGenerator = randomGenerator
+        bankOperationQueue.maxConcurrentOperationCount = bank.numberOfBankTeller
+    }
+
+    mutating func start() {
+        while shouldContinue() {
+            createRandomCustomer()
+            bank.openBank()
+            do {
+                try handleCustomer()
+                try bank.closeBank(totalCustomerNumber: totalCustomerNumber)
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    private mutating func createRandomCustomer() {
+        randomCustomers = randomGenerator.generateRandomCustomer()
+        totalCustomerNumber = randomGenerator.totalCustomer
+    }
+
+    private mutating func handleCustomer() throws {
+        guard var randomCustomers = randomCustomers else { throw BankManagerError.failToGenerateRandomCustomers }
+
+        randomCustomers.sort { $0.priority > $1.priority }
+
+//        randomCustomers.forEach {
+//            let customerOperation = HandleCustomerOperation(customer: $0)
+//            bankOperationQueue.addOperation(customerOperation)
+//        }
+
+        bankOperationQueue.waitUntilAllOperationsAreFinished()
+    }
+
+    private mutating func shouldContinue() -> Bool {
+        var result: Result<Bool, BankManagerError> = .success(true)
+        var shouldContinue = true
+
+        repeat {
+            consoleViewController.showStartMenu()
+            if case let .failure(error) = result {
+                print(error)
+                shouldContinue = false
+            }
+
+            if case let .success(shouldContinueResult) = result {
+                shouldContinue = shouldContinueResult
+            }
+        } while result == .failure(.invalidUserInput)
+
+        return shouldContinue
+    }
+}
